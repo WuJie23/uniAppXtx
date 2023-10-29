182786fda014d4f026583ff2ad8c5e5a9342ad25
wujie <2902976517@qq.com>
2023-10-29 13:51:35 +0800

    轮播开始

---
+++ src/components/XtxSwiper.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const activeIndex = ref(0)
+</script>
+
+<template>
+  <view class="carousel">
+    <swiper :circular="true" :autoplay="true" :interval="3000" :indicator-dots="true">
+      <swiper-item>
+        <navigator url="/pages/index/index" hover-class="none" class="navigator">
+          <image
+            mode="aspectFill"
+            class="image"
+            src="https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/slider_1.jpg"
+          ></image>
+        </navigator>
+      </swiper-item>
+      <swiper-item>
+        <navigator url="/pages/index/index" hover-class="none" class="navigator">
+          <image
+            mode="aspectFill"
+            class="image"
+            src="https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/slider_2.jpg"
+          ></image>
+        </navigator>
+      </swiper-item>
+      <swiper-item>
+        <navigator url="/pages/index/index" hover-class="none" class="navigator">
+          <image
+            mode="aspectFill"
+            class="image"
+            src="https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/slider_3.jpg"
+          ></image>
+        </navigator>
+      </swiper-item>
+    </swiper>
+  </view>
+</template>
+
+<style lang="scss" scoped>
+/* 轮播图 */
+.carousel {
+  height: 100%;
+  position: relative;
+  overflow: hidden;
+  transform: translateY(0);
+  background-color: #efefef;
+  .indicator {
+    z-index: 66666666666;
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 16rpx;
+    display: flex;
+    justify-content: center;
+    .dot {
+      width: 20rpx;
+      height: 20rpx;
+      margin: 0 8rpx;
+      border-radius: 50%;
+      background-color: rgba(0, 0, 0, 0.281);
+    }
+    .active {
+      background-color: #ffffff;
+    }
+  }
+  .navigator,
+  .image {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
-import CustomNavbar from '@/pages/index/components/CustomNavbar.vue'
+import CustomNavbar from '@/pages/index/component/CustomNavbar.vue'
+import XtxSwiper from '@/components/XtxSwiper.vue'
 </script>
 
 <template>
-  <CustomNavbar></CustomNavbar>
-  <view class="index">123</view>
+  <view class="CustomNavbar">
+    <CustomNavbar></CustomNavbar>
+  </view>
+
+  <view class="XtxSwiper">
+    <XtxSwiper></XtxSwiper>
+  </view>
 </template>
 
-<style lang="scss">
-//
+<style lang="scss" scoped>
+.CustomNavbar {
+}
+.XtxSwiper {
+  height: 300rpx;
+}
 </style>
